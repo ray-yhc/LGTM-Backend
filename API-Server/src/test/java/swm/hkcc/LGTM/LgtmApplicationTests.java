808191c1327774f2da09dceed067a1f8fd181800
ray-yhc <raycho521@yonsei.ac.kr>
2023-07-08 11:07:57 +0900

    feat: add restdocs settings

--- API-Server/src/test/java/swm/hkcc/LGTM/LgtmApplicationTests.java
+++ API-Server/src/test/java/swm/hkcc/LGTM/LgtmApplicationTests.java
@@ -2,12 +2,14 @@ package swm.hkcc.LGTM;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("dev")
 class LgtmApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
